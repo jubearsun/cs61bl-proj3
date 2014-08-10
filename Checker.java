6909778ecd457946bae8bb796294632f5f0f79ca
jubearsun <julia.sun@berkeley.edu>
2014-08-10 13:26:12 -0700

    Update Checker.java

--- Checker.java
+++ Checker.java
@@ -1,3 +1,5 @@
+
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.NoSuchFileException;
@@ -12,7 +14,7 @@ public class Checker {
 	
 	private Board myBoard;
 	private ArrayList<Block> myBlocks;
-	private int currentBlock = 1;
+	private int currentBlock = 1; 
 	
 	public static void main(String [] args) throws FileNotFoundException {
 		if (args.length != 2) {
@@ -56,7 +58,8 @@ public class Checker {
 					boardNBlockHelper.clear();
 				}
 				else if ((i - 2) % 4 == 0) {
-					Block blockToAdd = new Block(boardNBlockHelper);
+					Block blockToAdd = new Block(boardNBlockHelper, currentBlock);
+					currentBlock++;
 					myBlocks.add(blockToAdd);
 					boardNBlockHelper.clear();
 				}
@@ -104,7 +107,8 @@ public class Checker {
 					myCoors.add(gBlocks.nextInt());
 					myCoors.add(gBlocks.nextInt());
 					myCoors.add(gBlocks.nextInt());
-					rtnBlocks.add(new Block(myCoors));
+					rtnBlocks.add(new Block(myCoors, currentBlock));
+					currentBlock++;
 					myCoors.clear();
 				}
 			} catch (NoSuchElementException e) {
@@ -121,7 +125,7 @@ public class Checker {
 	
 	public void CheckGoal(ArrayList<Block> gBlocks) {
 		int blocksMatchedSoFar = 0;
-		ArrayList<Block> myBlocks = myBoard.blocks;
+		ArrayList<Block> myBlocks = myBoard.getBlocks();
 		for (Block b: gBlocks) {
 			for (Block checker: myBlocks) {
 				if (checker.equals(b)) {
@@ -143,254 +147,5 @@ public class Checker {
 		myBoard.printBoard();
 	}
 	
-	private class Board {
-		int[][] board;
-		int myHeight;
-		int myWidth;
-		ArrayList<Block> blocks;
-		
-		public Board(int height,int width) {
-			blocks = new ArrayList<Block>();
-			myHeight = height;
-			myWidth = width;
-			board = new int[height][width];
-		}
-		
-		public Block getBlock(int[] topLeft) {
-			for (int i = 0; i < blocks.size(); i++) {
-				Block currBlock = blocks.get(i);
-				if (topLeft[0] == currBlock.getTopLeftRow() && topLeft[1] == currBlock.getTopLeftCol()) {
-					return currBlock;
-				}
-			}
-			return null;
-		}
-		
-		private void createBoard() {
-			for (int i = 0; i < myBlocks.size(); i ++) {
-				Block currBlock = myBlocks.get(i);
-				if (isBlocked(currBlock)) {
-					System.out.println(5);
-					System.exit(5);
-				} else {
-					putBlock(currBlock);
-				}
-			}
-		}
-		
-		private boolean isBlocked(Block block) {
-			for (int a = block.getBottomRightRow(); a <= block.getTopLeftRow(); a++) {
-				for (int i = block.getTopLeftCol(); i <= block.getBottomRightCol(); i++) {
-					if (board[a][i] != 0) {
-						return true;
-					}
-				}
-			}
-			return false;
-		}
-		
-		private void putBlock(Block block) {
-			if (isBlocked(block)) {
-				System.out.println(6);
-				System.exit(6);
-			} else {
-				for (int a = block.getTopLeftRow(); a <= block.getBottomRightRow(); a++) {
-					for (int i = block.getTopLeftCol(); i <= block.getBottomRightCol(); i++) {
-						if (a >= myHeight || i > myWidth) {
-							System.exit(6);
-						} else {
-							board[a][i] = block.getBlock();
-						}
-					}
-				}
-			}
-			blocks.add(block);
-		}
-		
-		private void removeBlock(Block block) {
-			int blockIndicator = block.getBlock();
-			for (int i = 0; i < myHeight; i++) {
-				for (int a = 0; a < myWidth; a++) {
-					if (board[i][a] == blockIndicator) {
-						board[i][a] = 0;
-					}
-				}
-			}
-			blocks.remove(block);
-		}
-		
-		private void makeMove(int[] oldSpot, int[] newSpot) {
-			if (oldSpot[0] > myHeight - 1 || newSpot[0] > myHeight - 1 ||
-				oldSpot[1] > myWidth - 1 || newSpot[1] > myWidth - 1) {
-				System.out.println(6);
-				System.exit(6);
-			}
-			int blockIndicator = board[oldSpot[0]][oldSpot[1]];
-			if (blockIndicator == 0) {
-				System.out.println(6);
-				System.exit(6);
-			}
-			Block toBeRemoved = null;
-			Block toBeAdded;
-			ArrayList<Integer> toBeAddedCoors = new ArrayList<Integer>();
-			toBeAddedCoors.add(newSpot[0]);
-			toBeAddedCoors.add(newSpot[1]);
-			for (int i = 0; i < blocks.size(); i++) {
-				Block currBlock = blocks.get(i);
-				if (currBlock.getTopLeftRow() == oldSpot[0] && currBlock.getTopLeftCol() == oldSpot[1]) {
-					toBeRemoved = currBlock;
-				}
-			}
-			toBeAddedCoors.add(newSpot[0] + toBeRemoved.dimension[0] - 1);
-			toBeAddedCoors.add(newSpot[1] +toBeRemoved.dimension[1] - 1);
-			toBeAdded = new Block(toBeAddedCoors, blockIndicator);
-			this.removeBlock(toBeRemoved);
-			this.putBlock(toBeAdded);
-		}
-		
-		
-		public void printBoard() {
-			for (int i = 0; i < myHeight; i++) {
-				for (int a = 0; a < myWidth; a++) {
-					int myBlock = board[i][a];
-					System.out.print(myBlock + " ");
-				}
-				System.out.println();
-			}
-		}
-		
-		private boolean isLegalMove(Block old, int[] newSpot) {
-			int newRightCornerRow = old.getBottomRightRow() + (newSpot[0]-old.getTopLeftRow());
-			int newRightCornerCol = old.getBottomRightCol() + (newSpot[1]-old.getTopLeftCol());
-			return !((old.getTopLeftRow() > myHeight - 1 || newSpot[0] > myHeight - 1 ||
-					old.getTopLeftCol() > myWidth - 1 || newSpot[1] > myWidth - 1) || 
-					board[old.getTopLeftRow()][old.getTopLeftCol()] == 0 ||
-					(board[newSpot[0]][newSpot[1]] != 0 && 
-					(board[newRightCornerRow][newRightCornerCol] != 0 ||
-					board[newRightCornerRow][newRightCornerCol] != old.blockIndicator)
-					));
-		}
-		
-		public ArrayList<Board> generateMoves() {
-			ArrayList<Board> results = new ArrayList<Board>();
-			Board moveRight = this;
-			Board moveLeft = this;
-			Board moveUp = this;
-			Board moveDown = this;
-			for (Block block : this.blocks) {
-				if (isBlocked(block)) {
-					continue;
-				} else {
-					Block curr = block;
-					int[] toMove = block.topLeftCoor;
-					int[] right = new int[2];
-					right[0] = toMove[0];
-					right[1] = toMove[1] + 1;
-					int[] left = new int[2];
-					left[0] = toMove[0];
-					left[1] = toMove[1] - 1;
-					int[] up = new int[2];
-					right[0] = toMove[0] - 1;
-					right[1] = toMove[1];
-					int[] down = new int[2];
-					right[0] = toMove[0] + 1;
-					right[1] = toMove[1];
-					
-					if (isLegalMove(curr, right)) {
-						moveRight.makeMove(toMove, right);
-						results.add(moveRight);
-					} else if (isLegalMove(curr, left)) {
-						moveLeft.makeMove(toMove, left);
-						results.add(moveLeft);
-					} else if (isLegalMove(curr, up)) {
-						moveUp.makeMove(toMove, up);
-						results.add(moveUp);
-					} else if (isLegalMove(curr, down)) {
-						moveDown.makeMove(toMove, down);
-						results.add(moveDown);
-					}					
-				}
-			}
-			return results;
-		}
-	}
 	
-	public class Block {
-		private int[] topLeftCoor = new int[2];
-		private int[] bottomRightCoor = new int[2];
-		private int blockIndicator; //number that helps distinct one block from another.
-		private int[] dimension = new int[2];
-		
-		public Block (ArrayList<Integer> myCoors) {
-			blockIndicator = currentBlock;
-			currentBlock++;
-			if (myCoors.size() != 4) {
-				System.out.println(5);
-				System.exit(5);
-			} else {
-				topLeftCoor[0] = myCoors.get(0);
-				topLeftCoor[1] = myCoors.get(1);
-				bottomRightCoor[0] = myCoors.get(2);
-				bottomRightCoor[1] = myCoors.get(3);
-				makeDimension(myCoors);
-			}
-		}
-		
-		public Block (ArrayList<Integer> myCoors, int myBlockInd) {
-			System.out.println(myCoors.toString());
-			blockIndicator = myBlockInd;
-			if (myCoors.size() != 4) {
-				System.out.println(5);
-				System.exit(5);
-			} else {
-				topLeftCoor[0] = myCoors.get(0);
-				topLeftCoor[1] = myCoors.get(1);
-				bottomRightCoor[0] = myCoors.get(2);
-				bottomRightCoor[1] = myCoors.get(3);
-				makeDimension(myCoors);
-			}
-		}
-		
-		public void makeDimension(ArrayList<Integer> myCoors) {
-			dimension[0] = myCoors.get(2) - myCoors.get(0) + 1;
-			dimension[1] = myCoors.get(3) - myCoors.get(1) + 1;
-		}
-		
-		public int getBlock() {
-			return blockIndicator;
-		}
-		
-		public int getTopLeftRow() {
-			return topLeftCoor[0];
-		}
-		
-		public int getTopLeftCol() {
-			return topLeftCoor[1];
-		}
-		
-		public int getBottomRightRow() {
-			return bottomRightCoor[0];
-		}
-		
-		public int getBottomRightCol() {
-			return bottomRightCoor[1];
-		}
-		
-		public int[] getDimension() {
-			return dimension;
-		}
-		
-		public boolean equals(Block other) {
-			return (this.dimension[0] == other.dimension[0]) &&
-				   (this.dimension[1] == other.dimension[1]) &&
-				   (this.getTopLeftRow() == other.getTopLeftRow()) &&
-				   (this.getTopLeftCol() == other.getTopLeftCol()) &&
-				   (this.getBottomRightRow() == other.getBottomRightRow()) &&
-				   (this.getBottomRightCol() == other.getBottomRightCol());		   
-		}
-		
-		public String toString() {
-			return topLeftCoor[0] + " " + topLeftCoor[1] + " " + bottomRightCoor[0] + " " + bottomRightCoor[1];
-		}
-	}
 }
